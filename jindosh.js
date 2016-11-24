37784bebcbd29bea9b2ffa6a2493c8ad1480909a
Nikolai Svakhin <svakhine@gmail.com>
2016-11-24 19:17:38 +0000

    Printing engine for the permutations

--- jindosh.js
+++ jindosh.js
@@ -1,6 +1,10 @@
+'use strict';
+
 const allcombinations = require('allcombinations')
  
 console.log(Array.from(allcombinations([0, 1, 2, 3, 4])));
+
+const noPermutationIndexArray = [0, 1, 2, 3, 4];
 
 const ladyArray = [ "Doctor Marcolla", 
                     "Lady Winslow",
@@ -36,4 +40,68 @@ const heirloomArray = [ "Snuff Tin",
                         "Diamond" ];
     
 
+class Permutation {
+    constructor(itemsArray, indicesArray) {
+      this.itemsArray = itemsArray;
+      this.indicesArray = indicesArray;
+    }
     
+    at(i) {
+      const index = this.indicesArray[i];
+      return this.itemsArray[index];
+    }
+    
+    printable_at(i) {
+     var toPrint = ""; 
+     const stringv = this.at(i).toString();
+        if (stringv.length >= 8) {
+            toPrint += (stringv+"\t");
+        } else {
+            toPrint += (stringv+"\t\t");
+        }
+        return toPrint;
+    }
+}
+
+class SolutionCandidate {
+    constructor(ladyPermutation, spotPermutation, colorPermutation, originPermutation, drinkPermutation, heirloomPermutation) {
+        this.ladyPermutation = ladyPermutation;
+        this.spotPermutation = spotPermutation;
+        this.colorPermutation = colorPermutation;
+        this.originPermutation = originPermutation;
+        this.drinkPermutation = drinkPermutation;
+        this.heirloomPermutation = heirloomPermutation;
+    }
+    
+    Printable(property, i){
+        
+    }
+    Print() {
+     for (var i=0;i<5;i++) {
+         
+        var fullToPrint = "";
+        fullToPrint += this.ladyPermutation.printable_at(i);
+        fullToPrint += this.spotPermutation.printable_at(i);
+        fullToPrint += this.colorPermutation.printable_at(i);
+        fullToPrint += this.originPermutation.printable_at(i);
+        fullToPrint += this.drinkPermutation.printable_at(i);
+        fullToPrint += this.heirloomPermutation.printable_at(i);
+        
+        console.log(fullToPrint);
+        // console.log(this.ladyPermutation.at(i) + "\t" + this.spotPermutation.at(i) + "\t" + this.colorPermutation.at(i) + "\t" +this.originPermutation.at(i) + "\t" + this.drinkPermutation.at(i) + "\t" + this.heirloomPermutation.at(i));
+     }   
+    }
+    
+}
+
+const lp = new Permutation(ladyArray, noPermutationIndexArray);
+const sp = new Permutation(spotArray, noPermutationIndexArray);
+const cp = new Permutation(colorArray, noPermutationIndexArray);
+const op = new Permutation(originArray, noPermutationIndexArray);
+const dp = new Permutation(drinkArray, noPermutationIndexArray);
+const hp = new Permutation(heirloomArray, noPermutationIndexArray);
+
+const solution = new SolutionCandidate(lp, sp, cp, op, dp, hp);
+
+solution.Print();
+
