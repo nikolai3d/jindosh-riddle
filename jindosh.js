348b11637c5cfdf5fb50880d6a8273d6122f5e26
Nikolai Svakhin <svakhine@gmail.com>
2016-11-24 18:31:32 +0000

    Initial arrays typed in

---
+++ jindosh.js
@@ -0,0 +1,39 @@
+const allcombinations = require('allcombinations')
+ 
+console.log(Array.from(allcombinations([0, 1, 2, 3, 4])));
+
+const ladyArray = [ "Doctor Marcolla", 
+                    "Lady Winslow",
+                    "Madam Natsiou", 
+                    "Baroness Finch",
+                    "Countess Contee"];
+                    
+const spotArray = [ 1, 2, 3, 4, 5];
+
+const colorArray = [ "White", 
+                     "Red", 
+                     "Green", 
+                     "Blue", 
+                     "Purple"];
+
+const originArray = [ "Dabokva", 
+                      "Baleton",
+                      "Karnaca",
+                      "Dunwall",
+                      "Fraeport"];
+
+const drinkArray = [    "Absinthe",
+                        "Rum",
+                        "Wine",
+                        "Whiskey",
+                        "Beer"];
+    
+    
+const heirloomArray = [ "Snuff Tin",
+                        "Bird Pendant",
+                        "Ring",
+                        "War Medal",
+                        "Diamond" ];
+    
+
+    
